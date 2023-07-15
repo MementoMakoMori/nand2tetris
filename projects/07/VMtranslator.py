b8e1f50cb830fa3ac1210c90cc7d6fb5ff4ffbda
MementoMakoMori <rholley@u.rochester.edu>
2023-07-15 11:05:36 -0400

    tweaked Chapter 7 project, VMtranslator.py

--- projects/07/VMtranslator.py
+++ projects/07/VMtranslator.py
@@ -38,6 +38,7 @@ class Parser:
                 self.writer.set_curr_file(self.input.name)
                 self.advance()
                 self.hasMoreCommands()
+        self.input.close()
         return False
 
     def commandType(self):
@@ -62,63 +63,63 @@ class CodeWriter:
     def __init__(self, name):
         self.file = open("{}.asm".format(name), 'w')
         self.curr_file = None
+        self.num = 1
+        self.defs = {'inc': "\n".join(["@SP", "M=M+1"]), 'dec': "\n".join(["@SP", "M=M-1"]),
+                     'yes': "\n".join(["@SP", "A=M", "M=-1"]), 'no': "\n".join(["@SP", "A=M", "M=0"])}
+        self.defs.update({'push': "\n".join(["@SP", "A=M", "M=D", self.defs['inc']]), 'pop': "\n".join([self.defs['dec'], "A=M", "D=M"])})
 
     def set_curr_file(self, file: str):
         self.curr_file = get_filename(file)
 
-    def writeArithmetic(self, command: str, num: int=1):
-        num = str(num)
+    def writeArithmetic(self, command: str):
         asm = None
-        inc = "\n".join(["@SP", "M=M+1"])
-        dec = "\n".join(["@SP", "M=M-1"])
-        yes = "\n".join(["@SP", "A=M", "M=-1"])
-        no = "\n".join(["@SP", "A=M", "M=0"])
         if command == "neg":
-            asm = "\n".join([dec, "A=M", "M=-M"])
+            asm = "\n".join([self.defs['dec'], "A=M", "M=-M"])
         elif command == "not":
-            asm = "\n".join([dec, "A=M", "M=!M"])
+            asm = "\n".join([self.defs['dec'], "A=M", "M=!M"])
         else:
-            asm = "\n".join([dec, "A=M", "D=M", dec, "A=M"])
+            asm = "\n".join([self.defs['dec'], "A=M", "D=M", self.defs['dec'], "A=M"])
             match command:
                 case "add":
                     asm = "\n".join([asm, "M=D+M"])
                 case "sub":
                     asm = "\n".join([asm, "M=M-D"])
                 case "eq":
-                    asm = "\n".join([asm, "D=D-M", f"@EQ{num}", "D;JEQ", no, f"@EQINC{num}", "0;JMP", f"(EQ{num})", yes, f"(EQINC{num})"])
+                    asm = "\n".join([asm, "D=D-M", f"@EQ{self.num}", "D;JEQ", self.defs['no'], f"@EQINC{self.num}", "0;JMP", f"(EQ{self.num})", self.defs['yes'], f"(EQINC{self.num})"])
+                    self.num += 1
                 case "gt":
-                    asm = "\n".join([asm, "D=M-D", f"@GT{num}", "D;JGT", no, f"@GTINC{num}", "0;JMP", f"(GT{num})", yes, f"(GTINC{num})"])
+                    asm = "\n".join([asm, "D=M-D", f"@GT{self.num}", "D;JGT", self.defs['no'], f"@GTINC{self.num}", "0;JMP", f"(GT{self.num})", self.defs['yes'], f"(GTINC{self.num})"])
+                    self.num += 1
                 case "lt":
-                    asm = "\n".join([asm, "D=M-D", f"@LT{num}", "D;JLT", no, f"@LTINC{num}", "0;JMP", f"(LT{num})", yes, f"(LTINC{num})"])
+                    asm = "\n".join([asm, "D=M-D", f"@LT{self.num}", "D;JLT", self.defs['no'], f"@LTINC{self.num}", "0;JMP", f"(LT{self.num})", self.defs['yes'], f"(LTINC{self.num})"])
+                    self.num += 1
                 case "and":
                     asm = "\n".join([asm, "M=D&M"])
                 case "or":
                     asm = "\n".join([asm, "M=D|M"])
         assert asm is not None
-        self.file.write("\n".join([asm, inc, ""]))
+        self.file.write("\n".join([asm, self.defs['inc'], ""]))
 
     def writePushPop(self, command: str, segment: str, index: int):
         asm = None
-        push = "\n".join(["@SP", "A=M", "M=D", "@SP", "M=M+1"])
-        pop = "\n".join(["@SP", "M=M-1", "A=M", "D=M"])
         address = {"local": "@LCL", "argument": "@ARG", "this": "@THIS", "that": "@THAT",
                    "pointer": "@R"+str(3+index), "temp": "@R"+str(5+index), "constant": "@"+str(index),
                    "static": "@"+self.curr_file+"."+str(index)}
         if segment in ["local", "argument", "this", "that"]:
             match command:
                 case "push":
-                    asm = "\n".join(["@"+str(index), "D=A", address[segment], "A=D+M", "D=M", push])
+                    asm = "\n".join(["@"+str(index), "D=A", address[segment], "A=D+M", "D=M", self.defs['push']])
                 case "pop":
-                    asm = "\n".join(["@"+str(index), "D=A", address[segment], "D=D+M", "@R13", "M=D", pop, "@R13",
-                                     "A=M", "M=D"])
+                    asm = "\n".join(["@"+str(index), "D=A", address[segment], "D=D+M", "@R13", "M=D", self.defs['pop'],
+                                     "@R13", "A=M", "M=D"])
         if segment in ["pointer", "temp", "static"]:
             match command:
                 case "push":
-                    asm = "\n".join([address[segment], "D=M", push])
+                    asm = "\n".join([address[segment], "D=M", self.defs['push']])
                 case "pop":
-                    asm = "\n".join([pop, address[segment], "M=D"])
+                    asm = "\n".join([self.defs['pop'], address[segment], "M=D"])
         if segment == "constant":
-            asm = "\n".join([address[segment], "D=A", push])
+            asm = "\n".join([address[segment], "D=A", self.defs['push']])
         assert asm is not None
         self.file.write(asm+"\n")
 
@@ -126,14 +127,11 @@ class CodeWriter:
 if __name__ == "__main__":
     file = sys.argv[1]
     parse = Parser(file)
-    math_num = 1
     while parse.hasMoreCommands():
         if parse.commandType() == "C_ARITHMETIC":
-            parse.writer.writeArithmetic(parse.arg1(), math_num)
-            math_num += 1
+            parse.writer.writeArithmetic(parse.arg1())
         else:
             parse.writer.writePushPop(parse.current.split()[0], parse.arg1(), parse.arg2())
         parse.advance()
     parse.writer.file.close()
-    parse.input.close()
 
