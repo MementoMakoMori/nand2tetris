f96bec68cc2b8565acb0bd38678184964de7f455
R. Holley <rholley@u.rochester.edu>
2023-08-30 19:15:26 -0400

    ch 10 jack xml compiler completed!

--- projects/10/JackAnalyzer.py
+++ projects/10/JackAnalyzer.py
@@ -2,19 +2,17 @@ import re
 import glob
 import sys
 
-regleft = re.compile(r'(?<=\S)([.{}()\[\]+\-|=<>,;&~]|(?<![*/])[/*])')
-regright = re.compile(r'([.{}()\[\]+\-|=<>,;&~]|[/*](?![*/]))(?=\S)')
+# regleft = re.compile(r'(?<=\S)([.{}()\[\]+\-|=<>,;&~]|(?<![*/])[/*])')
+# regright = re.compile(r'([.{}()\[\]+\-|=<>,;&~]|[/*](?![*/]))(?=\S)')
 
 
 class JackTokenizer:
 
     def __init__(self, file):
         self.input = open(file, 'r')
-        # self.output = open(f"{file.split('.jack')[0]}T2.xml", 'a')
-        # self.output.write('<tokens>\n')
         self.line = None
         self.token = 'init'
-        self.symbols = {k: True for k in '{}()[].,;+-*/&|<>=-~'}
+        self.symbols = {k: True for k in '{}()[].,;+-*/&|<>=-~'} # replace with hash set
         self.kws = {k: k.upper() for k in ['class', 'constructor', 'function', 'method', 'field', 'static', 'var',
                                            'int', 'char', 'boolean', 'void', 'true', 'false', 'null', 'this', 'let',
                                            'do', 'if', 'else', 'while', 'return']}
@@ -62,7 +60,6 @@ class JackTokenizer:
                     tokens.append(self.line[left:right])
                     left = right
             self.line = tokens
-            # print(self.line)
 
     def advance(self):
         if self.line:
@@ -80,60 +77,32 @@ class JackTokenizer:
             return True
         else:
             self.input.close()
-            # self.output.write('</tokens>\n')
-            # self.output.close()
             return False
 
     def tokenType(self) -> str:
         if self.kws.get(self.token, None):
-            # self.output.write(f'<keyword> {self.token} </keyword>\n')
             return 'keyword'
         elif self.symbols.get(self.token, None):
-            # match self.token:
-            #     case '<':
-            #         self.output.write('<symbol> &lt; </symbol>\n')
-            #     case '>':
-            #         self.output.write('<symbol> &gt; </symbol>\n')
-            #     case '&':
-            #         self.output.write('<symbol> &amp; </symbol>\n')
-            #     case _:
-            #         self.output.write(f'<symbol> {self.token} </symbol>\n')
             return 'symbol'
         else:
             try:
                 if 0 <= int(self.token) <= 32767:
-                    # self.output.write(f'<integerConstant> {self.token} </integerConstant>\n')
                     return 'integerConstant'
             except ValueError:
                 pass
         if len(self.token) > 1 and ((self.token[0] and self.token[-1]) == '"'):
-            # self.output.write(f'<stringConstant> {self.token[1:-1]} </stringConstant>\n')
             return 'stringConstant'
         else:
-            # self.output.write(f'<identifier> {self.token} </identifier>\n')
             return 'identifier'
 
-    def keyWord(self):
-        return self.token
-
-    def symbol(self):
-        match self.token:
-            case '<':
-                return '&lt;'
-            case '>':
-                return '&gt;'
-            case '&':
-                return '&amp;'
-        return self.token
-
-    def identifier(self):
-        return self.token
-
-    def intVal(self):
-        return int(self.token)
-
-    def stringVal(self):
-        return self.token[1:-1]
+    def tokenValue(self, tok_type):
+        match tok_type:
+            case 'integerConstant':
+                return int(self.token)
+            case 'stringConstant':
+                return self.token[1:-1]
+            case _:
+                return self.token
 
 
 class CompilationEngine:
@@ -142,28 +111,26 @@ class CompilationEngine:
         self.tk = JackTokenizer(file)
         self.output = open(f"{file.split('.jack')[0]}Mine.xml", 'a')
         self.tab = 0
-        self.tok = {'type': '', 'token': ''}
+        self.tok = {'type': '', 'value': ''}
+        self.check = {'ops': {x for x in '+-*/&|<>='}, 'unary': {y for y in '-~'}, 'kws': {z for z in ['true', 'false', 'null', 'this']}}
         self.compileClass()
 
     def nextToken(self):
         if self.tk.hasMoreTokens():
             self.tok['type'] = self.tk.tokenType()
-            match self.tok['type']:
-                case 'keyword':
-                    self.tok['token'] = self.tk.keyWord()
-                case 'symbol':
-                    self.tok['token'] = self.tk.symbol()
-                case 'identifier':
-                    self.tok['token'] = self.tk.identifier()
-                case 'integerConstant':
-                    self.tok['token'] = self.tk.intVal()
-                case 'stringConstant':
-                    self.tok['token'] = self.tk.stringVal()
+            self.tok['value'] = self.tk.tokenValue(self.tok['type'])
             self.tk.advance()
 
     def writeTok(self):
-        self.output.write(('  '*self.tab)+f'<{self.tok["type"]}> {self.tok["token"]} </{self.tok["type"]}>\n')
+        self.output.write(('  '*self.tab)+f'<{self.tok["type"]}> {self.tok["value"]} </{self.tok["type"]}>\n')
         self.nextToken()
+
+    def checkTok(self, check_type: str = None, check_val: str = None):
+        if check_type and self.tok['type'] != check_type:
+            raise SyntaxError(f"Unexpected token type: {self.tok['value']} of type {self.tok['type']}")
+        if check_val and self.tok['value'] != check_val:
+            raise SyntaxError(f"Unexpected token value: {self.tok['value']} of type {self.tok['type']}")
+        self.writeTok()
 
     def writeTag(self, tag: str, start: bool):
         if start:
@@ -176,23 +143,17 @@ class CompilationEngine:
     def compileClass(self):
         self.writeTag('class', True)
         self.nextToken()
-        assert self.tok['token'] == 'class'
-        self.writeTok()
+        self.checkTok(check_val='class')
+        self.checkTok(check_type='identifier')
+        self.checkTok(check_val='{')
         
-        assert self.tok['type'] == 'identifier'
-        self.writeTok()
-        
-        assert self.tok['token'] == '{'
-        self.writeTok()
-        
-        while self.tok['token'] in ['static', 'field']:
+        while self.tok['value'] in ['static', 'field']:
             self.compileClassVarDec()          
             
-        while self.tok['token'] in ['constructor', 'function', 'method']:
+        while self.tok['value'] in ['constructor', 'function', 'method']:
             self.compileSubroutine()
-            
-        assert self.tok['token'] == '}'
-        self.writeTok()
+
+        self.checkTok(check_val='}')
         self.writeTag('class', False)
         self.output.close()
 
@@ -202,57 +163,49 @@ class CompilationEngine:
         
         self.writeTok()  # no checking on the type because it could be a custom identifier
         # var name
-        assert self.tok['type'] == 'identifier'
-        self.writeTok()
+        self.checkTok(check_type='identifier')
+
         # if there is a comma + more var names
-        while self.tok['token'] == ',':
+        while self.tok['value'] == ',':
             self.writeTok()
-            assert self.tok['type'] == 'identifier'
-            self.writeTok()
+            self.checkTok(check_type='identifier')
+
         # list of var names ended (or never existed)
-        assert self.tok['token'] == ';'
-        self.writeTok()
+        self.checkTok(check_val=';')
         self.writeTag('classVarDec', False)
 
     def compileSubroutine(self):
         self.writeTag('subroutineDec', True)
         self.writeTok()  # checked for constructor, method, function, before calling
         
-        # later: check this is void, int, char, boolean, or type == 'identifier'
+        # return type
         self.writeTok()
-        
-        assert self.tok['type'] == 'identifier'
-        self.writeTok()
-        
-        assert self.tok['token'] == '('
-        self.writeTok()
-        
+        # subroutine name and optional params
+        self.checkTok(check_type='identifier')
+        self.checkTok(check_val='(')
         self.compileParameterList()
-        assert self.tok['token'] == ')'
-        self.writeTok()
+        self.checkTok(check_val=')')
         
         self.writeTag('subroutineBody', True)
-        assert self.tok['token'] == '{'
-        self.writeTok()
-        while self.tok['token'] == 'var':
+        self.checkTok(check_val='{')
+        while self.tok['value'] == 'var':
             self.compileVarDec()
-
         self.compileStatements()
-        assert self.tok['token'] == '}'
-        self.writeTok()
+        self.checkTok(check_val='}')
+
         self.writeTag('subroutineBody', False)
         self.writeTag('subroutineDec', False)
     
     def compileParameterList(self):
         self.writeTag('parameterList', True)
-        while self.tok['token'] != ')':
+
+        while self.tok['value'] != ')':
             while True:
                 self.writeTok()  # parameter type
                 # parameter name
-                assert self.tok['type'] == 'identifier'
-                self.writeTok()
+                self.checkTok(check_type='identifier')
                 # if there are no more parameters, break the loop
-                if self.tok['token'] != ',':
+                if self.tok['value'] != ',':
                     break
                 self.writeTok()
                 
@@ -263,22 +216,19 @@ class CompilationEngine:
         self.writeTok()  # already checked this was 'var'
         self.writeTok()  # var type
         # var name
-        assert self.tok['type'] == 'identifier'
-        self.writeTok()
+        self.checkTok(check_type='identifier')
         # write commas and var names if they exist
-        while self.tok['token'] == ',':
+        while self.tok['value'] == ',':
             self.writeTok()
-            assert self.tok['type'] == 'identifier'
-            self.writeTok()
+            self.checkTok(check_type='identifier')
         # end declaration
-        assert self.tok['token'] == ';'
-        self.writeTok()
+        self.checkTok(check_val=';')
         self.writeTag('varDec', False)
 
     def compileStatements(self):
         self.writeTag('statements', True)
-        while self.tok['token'] != '}':
-            match self.tok['token']:
+        while self.tok['value'] != '}':
+            match self.tok['value']:
                 case 'let':
                     self.compileLet()
                 case 'if':
@@ -294,103 +244,139 @@ class CompilationEngine:
     def compileDo(self):
         self.writeTag('doStatement', True)
         self.writeTok()
-        assert self.tok['type'] == 'identifier'
-        self.writeTok()
-        if self.tok['token'] == '.':
+
+        self.checkTok(check_type='identifier')
+        if self.tok['value'] == '.':
             self.writeTok()
-            assert self.tok['type'] == 'identifier'
-            self.writeTok()
-        assert self.tok['token'] == '('
-        self.writeTok()
+            self.checkTok(check_type='identifier')
+        self.checkTok(check_val='(')
         self.compileExpressionList()
-        assert self.tok['token'] == ')'
-        self.writeTok()
-        assert self.tok['token'] == ';'
-        self.writeTok()
+        self.checkTok(check_val=')')
+        self.checkTok(check_val=';')
         self.writeTag('doStatement', False)
 
     def compileLet(self):
         self.writeTag('letStatement', True)
         self.writeTok()
-        assert self.tok['type'] == 'identifier'
-        self.writeTok()
-        # HERE: ADD EXPRESSION HANDLING
-        assert self.tok['token'] == '='
-        self.writeTok()
+
+        self.checkTok(check_type='identifier')
+        if self.tok['value'] == '[':
+            self.writeTok()
+            self.compileExpression()
+            self.checkTok(check_val=']')
+        self.checkTok(check_val='=')
         self.compileExpression()
-        assert self.tok['token'] == ';'
-        self.writeTok()
+        self.checkTok(check_val=';')
+
         self.writeTag('letStatement', False)
 
     def compileWhile(self):
         self.writeTag('whileStatement', True)
         self.writeTok()
-        assert self.tok['token'] == '('
-        self.writeTok()
+
+        self.checkTok(check_val='(')
         self.compileExpression()
-        assert self.tok['token'] == ')'
-        self.writeTok()
-        assert self.tok['token'] == '{'
-        self.writeTok()
+        self.checkTok(check_val=')')
+
+        self.checkTok(check_val='{')
         self.compileStatements()
-        assert self.tok['token'] == '}'
-        self.writeTok()
+        self.checkTok(check_val='}')
+
         self.writeTag('whileStatement', False)
 
     def compileReturn(self):
         self.writeTag('returnStatement', True)
         self.writeTok()
-        if self.tok['token'] != ';':
+
+        if self.tok['value'] != ';':
             self.compileExpression()
-        assert self.tok['token'] == ';'
-        self.writeTok()
+        self.checkTok(check_val=';')
+
         self.writeTag('returnStatement', False)
 
     def compileIf(self):
         self.writeTag('ifStatement', True)
         self.writeTok()
-        assert self.tok['token'] == '('
-        self.writeTok()
+
+        self.checkTok(check_val='(')
         self.compileExpression()
-        assert self.tok['token'] == ')'
-        self.writeTok()
-        assert self.tok['token'] == '{'
-        self.writeTok()
+        self.checkTok(check_val=')')
+
+        self.checkTok(check_val='{')
         self.compileStatements()
-        assert self.tok['token'] == '}'
-        self.writeTok()
-        if self.tok['token'] == 'else':
+        self.checkTok(check_val='}')
+
+        if self.tok['value'] == 'else':
             self.writeTok()
-            assert self.tok['token'] == '{'
-            self.writeTok()
+            self.checkTok(check_val='{')
             self.compileStatements()
-            assert self.tok['token'] == '}'
-            self.writeTok()
+            self.checkTok(check_val='}')
+
         self.writeTag('ifStatement', False)
 
     def compileExpression(self):
         self.writeTag('expression', True)
+
         self.compileTerm()
+        while self.tok['value'] in self.check['ops']:
+            match self.tok['value']:
+                case '<':
+                    self.tok['value'] = '&lt;'
+                case '>':
+                    self.tok['value'] = '&gt;'
+                case '&':
+                    self.tok['value'] = '&amp;'
+            self.writeTok()
+            self.compileTerm()
+
         self.writeTag('expression', False)
 
     def compileTerm(self):
         self.writeTag('term', True)
-        self.writeTok()
+
+        if self.tok['value'] == '(':
+            self.writeTok()
+            self.compileExpression()
+            self.checkTok(check_val=')')
+
+        elif self.tok['value'] in self.check['unary']:
+            self.writeTok()
+            self.compileTerm()
+        elif self.tok['type'] in ['integerConstant', 'stringConstant'] or self.check['value'] in self.check['kws']:
+            self.writeTok()
+        elif self.tok['type'] == 'identifier':
+            self.writeTok()
+            if self.tok['value'] == '[':
+                self.writeTok()
+                self.compileExpression()
+                self.checkTok(check_val=']')
+
+            elif self.tok['value'] == '.':
+                self.writeTok()
+                self.checkTok(check_type='identifier')
+
+            if self.tok['value'] == '(':
+                self.writeTok()
+                self.compileExpressionList()
+                self.checkTok(check_val=')')
+
         self.writeTag('term', False)
 
     def compileExpressionList(self):
         self.writeTag('expressionList', True)
-        while self.tok['token'] != ')':
+
+        while self.tok['value'] != ')':
             self.compileExpression()
-            if self.tok['token'] != ',':
+            if self.tok['value'] != ',':
                 break
             self.writeTok()  # write the comma and continue the loop to compile the next expression
+
         self.writeTag('expressionList', False)
 
 
 if __name__ == '__main__':
     source = sys.argv[1]
     jacks = [x for x in glob.glob(source + '/*.jack')]
-    print(jacks)
+    # print(jacks)
     for jack in jacks:
         parser = CompilationEngine(jack)
